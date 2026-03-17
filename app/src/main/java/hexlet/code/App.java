92b2dbf2cddd2104b2b0320edc0b68309af6e475
Artem Maksimenko <mak5imenkoart@yandex.ru>
2026-03-17 23:51:30 +0300

    create package hexlet.code.game, create Engine.java and Calc.java, update app java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,8 @@
 package hexlet.code;
 
+import hexlet.code.game.Calc;
+import hexlet.code.game.Even;
+
 import java.util.Scanner;
 
 
@@ -9,17 +12,25 @@ public class App {
         System.out.println("Please enter the game number and press Enter.\n"
                 + "1 - Greet\n"
                 + "2 - Even\n"
+                + "3 - Calc\n"
                 + "0 - Exit");
         int num = numGame.nextInt();
         System.out.println("Your choice: " + num);
 
-        if (num == 1) {
-            System.out.println("Welcome to the Brain Games!");
-            Cli.gretting();
-        } else if (num == 2) {
-            System.out.println("Welcome to the Brain Games!");
-            Even.even();
+        switch (num) {
+            case "1":
+                System.out.println("Welcome to the Brain Games!");
+                Cli.gretting();
+                break;
+            case "2":
+                System.out.println("Welcome to the Brain Games!");
+                Even.even();
+                break;
+            case "3":
+                System.out.println("Welcome to the Brain Games!");
+                Calc.calc();
         }
+
         numGame.close();
     }
 }
