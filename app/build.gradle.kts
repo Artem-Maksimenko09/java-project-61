2ca4f82949a0a6ae8e61c31d8ea2514413011f76
Artem Maksimenko <mak5imenkoart@yandex.ru>
2026-03-31 22:47:34 +0300

    update

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id ("application")
+    checkstyle
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id "org.sonarqube" version "7.2.3.7755"
+}
+
+sonar {
+    properties {
+        property "sonar.projectKey", "Artem-Maksimenko09_java-project-612"
+        property "sonar.organization", "artem-maksimenko09"
+    }
+}
+
+
+application { mainClass.set("hexlet.code.App") }
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
